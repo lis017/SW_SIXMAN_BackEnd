4e0b8e1087593b1f680650f4a07b1f3b1a412c6d
MinHyeok Lee <minhyeok12.dev@gmail.com>
2025-05-28 10:53:55 +0900

    프론트와 백엔드 오류없이 연결된 버전

--- src/main/java/com/sixthMan/smartFarm/Plants/testController.java
+++ src/main/java/com/sixthMan/smartFarm/Plants/testController.java
@@ -2,11 +2,17 @@ package com.sixthMan.smartFarm.Plants;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class testController {
     @GetMapping("/write")
     String write(){
         return "write.html";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index.html";
     }
 }
